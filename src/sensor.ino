501ebfa1facae733c10d507b56e8f825ac5ff5b1
CarlosAndresErazoGarzon <carlos-erazo@javeriana.edu.co>
2022-04-04 04:43:43 -0500

    fixes changes

--- src/sensor.ino
+++ src/sensor.ino
@@ -1,8 +1,8 @@
 /*
  * Project Hydroponic-project
- * Description:
+ * Description: For reading 6 sensors
  * Author:
- * Date: 27/02/2022
+ * Date: 04/04/2022
  */
 
 #include "Adafruit_DHT.h"
@@ -10,56 +10,49 @@
 #include "Ubidots.h"
 #include "SdFat.h"
 
-SYSTEM_MODE(MANUAL);
+SYSTEM_MODE(MANUAL);	//Working offline
 
-// TEMPERATURE
-
-#define DHT8PIN 8
-#define DHT7PIN 7
-//#define DHT21PIN 6
-#define DHTYPE11 DHT11
 #define DHTYPE22 DHT22
+#define DHT8PIN 8	//First dht22 sensor on D8
+#define DHT7PIN 7	//Second dht22 sensor on D7
+#define HYGROD5 D5	//Digital input for the first yl100 sensor
+#define HYGROA1 A1	//Analog input for the first yl100 sensor
+#define HYGROD4 D4	//Digital input for the second yl100 sensor
+#define HYGROA2 A2	//Analog input for the second yl100 sensor
+#define GAA4 A4	//First light sensor
+#define GAA3 A3	//Second light sensor
 
-DHT dht1(DHT8PIN, DHTYPE11);
+DHT dht1(DHT8PIN, DHTYPE22);
 DHT dht2(DHT7PIN, DHTYPE22);
-
-#define DHT1SIZE 2
-#define DHT2SIZE 1
-
-float dht1Temp = 0;
-float dht1Hum = 0;
-
-float dht2Temp = 0;
-float dht2Hum = 0;
-
-// HYGROMETER
-
-#define HYGROD5 D5
-#define HYGROA1 A1
-#define HYGROD4 D4
-#define HYGROA2 A2
-
-int hygro1Hum = 0;
-int hygro2hum = 0;
-
-// LUX
-
-GA1A12S202 lux2(A4);
-GA1A12S202 lux3(A3);
-
-float lux1lux = 0;
-float lux2lux = 0;
+GA1A12S202 lux1(GAA4);
+GA1A12S202 lux2(GAA3);
 
 // Primary SPI with DMA
 // SCK => D13, MISO => D12, MOSI => D11, SS => D14/A5 (default)
 SdFat sd;
 const uint8_t chipSelect = SS;
-
 File myFile;
 
-void saveData(String text)
+float dht1Temp;
+float dht1Hum;
+float dht2Temp;
+float dht2Hum;
+int hygro1Hum;
+int hygro2hum;
+float lux1lux;
+float lux2lux;
+uint64_t argon_time;
+
+//Save data to sd
+void saveData()
 {
-	delay(5000);
+	//BOARDTIME HYGRO1 HYGRO2 HUM1 TEMP1 HUM2 TEMP2 LUX1 LUX2
+	String time_board = "%lu", argon_time;
+	String text = time_board + "," +
+				  String(hygro1Hum) + "," + String(hygro2hum) + "," +
+				  String(dht1Hum) + "," + String(dht1Temp) + "," +
+				  String(dht2Hum) + "," + String(dht2Temp) + "," +
+				  String(lux1lux) + "," + String(lux2lux);
 
 	if (!sd.begin(chipSelect, SPI_FULL_SPEED))
 	{
@@ -68,7 +61,7 @@ void saveData(String text)
 
 	if (!myFile.open("data.csv", O_RDWR | O_CREAT | O_AT_END))
 	{
-		sd.errorHalt("opening test.txt for write failed");
+		sd.errorHalt("Opening test.txt for write failed");
 	}
 
 	Serial.print("Writing to data.csv");
@@ -77,7 +70,7 @@ void saveData(String text)
 	myFile.println(text);
 
 	myFile.close();
-	Serial.println("\ndone.");
+	Serial.println("\nDone.");
 }
 
 void setup()
@@ -94,10 +87,7 @@ void setup()
 
 void loop()
 {
-
-	bool cent = true;
-	int contCent = 0;
-
+	//Reset
 	hygro1Hum = 0;
 	hygro2hum = 0;
 	dht1Hum = 0;
@@ -107,51 +97,15 @@ void loop()
 	lux1lux = 0;
 	lux2lux = 0;
 
-	// This number will overflow (go back to zero), after approximately 49 days
-	unsigned long time = millis();
-
+	//Read
+	argon_time = System.millis();
 	hygro1Hum = analogRead(HYGROA1);
 	hygro2hum = analogRead(HYGROA2);
-
-	//Rectify that the data is read
-	while (cent)
-	{
-		if (dht1Hum != 0 && dht1Temp != 0)
-		{
-			break;
-		}
-		else if (dht1Hum == 0)
-		{
-			delay(500);
-			dht1Hum = dht1.getHumidity();
-		}
-		else if (dht1Temp == 0)
-		{
-			delay(500);
-			dht1Temp = dht1.getTempCelcius();
-		}
-		contCent++;
-		if (contCent == 5)
-		{
-			cent = false;
-		}
-	}
-
 	dht2Hum = dht2.getHumidity();
 	dht2Temp = dht2.getTempCelcius();
-	lux1lux = lux2.getLux(true);
-	lux2lux = lux3.getLux(true);
+	lux1lux = lux1.getLux(true);
+	lux2lux = lux2.getLux(true);
 
-	//BOARDTIME HYGRO1 HYGRO2 HUM1 TEMP1 HUM2 TEMP2 LUX1 LUX2
-	String text = String(time) + "," +
-				  String(hygro1Hum) + "," + String(hygro2hum) + "," +
-				  String(dht1Hum) + "," + String(dht1Temp) + "," +
-				  String(dht2Hum) + "," + String(dht2Temp) + "," +
-				  String(lux1lux) + "," + String(lux2lux);
-
-	//Save the data on the SD card
-	saveData(text);
-
-	delay(900);
-	//delay(900000);
+	saveData();
+	delay(900000);	//15 min.
 }
